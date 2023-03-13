d7f76e91213121f243fc01ecad7f0896a1bdf01c
Goudjou Borel <goudjouborel@gmail.com>
2023-03-13 01:11:32 +0100

    Creer la base de données la parcourire et afficher les elements

--- fruitshop/products/models.py
+++ fruitshop/products/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+
+    nom  = models.CharField(max_length=200)
+    prix = models.CharField(max_length=200)
+    stock = models.CharField(max_length=200)
+    image = models.CharField(max_length=500)
+    date  = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.nom
+
